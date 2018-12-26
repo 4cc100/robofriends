4e477ea1bef2c93b1b0f2ae0f0e74bc46a584765
4cc100 <vikcsi.varga@gmail.com>
2018-12-26 22:10:50 +0100

    switch from static data to remote

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox.jsx';
-import { robots } from './robots';
 import './App.css'
 
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      robots: robots,
+      robotok: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robotok: users}));
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value});
   }
   
-  render(){
-    const filteredRobots = this.state.robots.filter((robot) => {
+  render() {
+    const filteredRobots = this.state.robotok.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
     return(
